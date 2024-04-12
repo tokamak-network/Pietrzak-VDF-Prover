70e933fd10e98e871452c3eba1f769745c960549
ijaheun <heun6030@naver.com>
2024-04-12 12:29:54 +0900

    update vdf-prover

--- golang/vdf-prover/cmd/main.go
+++ golang/vdf-prover/cmd/main.go
@@ -8,7 +8,7 @@ import (
 	"time"
 )
 
-type Claim struct {
+type claim struct {
 	N *big.Int
 	X *big.Int
 	Y *big.Int
@@ -16,20 +16,17 @@ type Claim struct {
 	V *big.Int
 }
 
-func main() {
-
-	// 2^21 ~ 2^25
-	// 2097152, 4194304, 8388608, 16777216, 33554432
-	T := 2097152
-	bits := 512 // 소수를 생성할 비트 수
+func setup(bits int) (*big.Int, *big.Int) {
 	N, err := util.GeneratePrime(bits)
 	if err != nil {
 		fmt.Println("Error generating prime:", err)
-		return
 	}
 	x := big.NewInt(243)
-	x.Mod(x, N)
+	x.Mod(x, N) // Ensure x < N
+	return N, x
+}
 
+func evaluate(N, x *big.Int, T int) ([]prover.Claim, prover.Claim) {
 	evalStart := time.Now()
 	y, expList := util.CalExp(N, x, T)
 
@@ -47,36 +44,45 @@ func main() {
 	proofList := []prover.Claim{claim}
 	evalExecTime := time.Since(evalStart)
 	fmt.Printf("Evaluation Time: %s\n", evalExecTime)
+
+	return proofList, claim
+}
+
+func genProof(claim prover.Claim) []prover.Claim {
 	start := time.Now()
-	proofList = prover.RecHalveProof(claim)
-
-	//for i, proof := range proofList {
-	//	fmt.Printf("proofList[%d]: (N: %s, X: %s, Y: %s, T: %d, V: %s)\n", i, proof.N, proof.X, proof.Y, proof.T, proof.V)
-	//}
-
+	proofList := prover.RecHalveProof(claim)
 	execTime := time.Since(start)
 	fmt.Printf("Execution time for generating and halving proof: %s\n", execTime)
+	return proofList
+}
 
+func verify(proofList []prover.Claim) bool {
 	startVerify := time.Now()
-
 	isVerify := prover.VerifyProof(proofList)
+	verifyTime := time.Since(startVerify)
+
+	fmt.Printf("Execution time for verification: %s\n", verifyTime)
+	return isVerify
+}
+
+func main() {
+	// 2^21 ~ 2^25
+	// 2097152, 4194304, 8388608, 16777216, 33554432
+	T := 4194304
+
+	// lambda
+	bits := 512
+
+	N, x := setup(bits)
+	proofList, claim := evaluate(N, x, T)
+
+	proofList = genProof(claim)
+
+	isVerify := verify(proofList)
+
 	if isVerify {
 		fmt.Println("Verification succeeded.")
 	} else {
 		fmt.Println("Verification failed.")
 	}
-
-	verifyTime := time.Since(startVerify)
-	fmt.Printf("Execution time for verification: %s\n", verifyTime)
-}
-
-func joinStrings(strs []string, sep string) string {
-	var result string
-	for i, s := range strs {
-		if i > 0 {
-			result += sep + ""
-		}
-		result += s
-	}
-	return result
 }
