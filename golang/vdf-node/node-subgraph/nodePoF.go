06ea4f9652aa004a9439735863e5bba44d4baf0a
Jaheun <heun6030@korea.ac.kr>
2024-07-04 15:34:11 +0900

    modify node process

--- golang/vdf-node/node-subgraph/nodePoF.go
+++ golang/vdf-node/node-subgraph/nodePoF.go
@@ -535,17 +535,17 @@ func containsRound(rounds []string, round string) bool {
 //}
 
 func (l *PoFClient) ProcessRoundResults() error {
-	//config := GetConfig()
-	//isOperator, err := IsOperator(config.WalletAddress)
-	//if err != nil {
-	//	log.Printf("Error fetching isOperator results: %v", err)
-	//	return err
-	//}
-	//
-	//if !isOperator {
-	//	ctx := context.Background()
-	//	l.OperatorDeposit(ctx)
-	//}
+	config := GetConfig()
+	isOperator, err := IsOperator(config.WalletAddress)
+	if err != nil {
+		log.Printf("Error fetching isOperator results: %v", err)
+		return err
+	}
+
+	if !isOperator {
+		ctx := context.Background()
+		l.OperatorDeposit(ctx)
+	}
 
 	results, err := l.GetRandomWordRequested()
 	if err != nil {
@@ -918,12 +918,12 @@ func IsOperator(operator string) (bool, error) {
 	client := graphql.NewClient(config.SubgraphURL)
 
 	req := graphql.NewRequest(`
-		query MyQuery($operator: String!) {
-			operatorNumberChangeds(where: {operator: $operator}) {
-				isOperator
-			}
-		}
-	`)
+        query MyQuery($operator: String!) {
+            operatorNumberChangeds(where: {operator: $operator}) {
+                isOperator
+            }
+        }
+    `)
 
 	req.Var("operator", operator)
 
@@ -934,8 +934,17 @@ func IsOperator(operator string) (bool, error) {
 	ctx := context.Background()
 	if err := client.Run(ctx, req, &respData); err != nil {
 		log.Printf("Failed to execute query: %v", err)
+
+		// HTML 응답인지 확인하기 위해 err 내용을 출력
+		if strings.Contains(err.Error(), "<") {
+			fmt.Println("Received HTML response, likely an error page")
+		}
+
 		return false, err
 	}
+
+	// 응답 데이터 출력
+	log.Printf("Response data: %+v", respData)
 
 	for _, record := range respData.OperatorNumberChangeds {
 		return record.IsOperator, nil
